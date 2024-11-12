b3e978ca4c55221958bf07b2691db18b56935a53
NadiaMHB <nhb_02@hotmail.com>
2024-11-11 22:55:55 -0500

    first commit

---
+++ src/main/java/org/example/lab9gtics/Lab9GticsApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab9gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab9GticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab9GticsApplication.class, args);
+	}
+
+}
